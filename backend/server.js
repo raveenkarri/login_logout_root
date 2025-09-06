b94702a0d65231ead5425ef43ab5383d03caae3d
raveenkarri <raveenkarri@gmail.com>
2025-09-06 19:27:31 +0530

    cors problem solution

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://login-logout-root.vercel.app/",
     credentials: true,
   })
 );
